dfcc5f0d14d75a6f08956780e65e1e394f63ac67
zxionf <zxionf@qq.com>
2026-04-29 23:47:01 +0800

    fix: paddingTop & icons

--- src/components/basePage.tsx
+++ src/components/basePage.tsx
@@ -1,5 +1,6 @@
 import { View } from "@tarojs/components"
 import { PropsWithChildren } from "react"
+import { getSafeArea } from "../utils/safeArea"
 
 interface SafeAreaViewProps {
     className?: string
@@ -12,7 +13,8 @@ export default function BasePage({
     fullScreen = false
 }: PropsWithChildren<SafeAreaViewProps>) {
     const isH5 = process.env.TARO_ENV === 'h5'
-
+    const { top: cachedTop, bottom: cachedBottom } = getSafeArea();
+    const paddingtop = cachedTop
     return (
         <View
             className={className}
@@ -20,7 +22,8 @@ export default function BasePage({
                 fullScreen
                     ? {
                         height: isH5 ? '100dvh' : '100%',
-                        paddingTop: 'env(safe-area-inset-top)',
+                        // paddingTop: 'env(safe-area-inset-top)',
+                        paddingTop: `${paddingtop}px`,
                         // 底部 = 原生安全区 + 自定义TabBar高度
                         paddingBottom: 'calc(env(safe-area-inset-bottom) + var(--tab-bar-height, 0px))',
                         // 低版本兼容
