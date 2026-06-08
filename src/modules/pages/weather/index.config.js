490270763acf2ab88b41606b25a8ec52701da14d
Spinach <super_spinach@qq.com>
2026-06-08 18:41:14 +0800

    feat: add weather page config

---
+++ src/modules/pages/weather/index.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+export default definePageConfig({
+  navigationBarTitleText: "天气",
+  navigationStyle: "custom",
+  enablePullDownRefresh: true,
+  backgroundColor: "#47a5fd",
+  backgroundTextStyle: "light",
+});
+
