ed0e7c23b55750deea1d166df36830a8db71e714
Spinach <super_spinach@qq.com>
2026-06-10 18:42:28 +0800

    fix: 设置首页刷新背景颜色

--- src/pages/index/index.config.js
+++ src/pages/index/index.config.js
@@ -3,5 +3,7 @@ export default definePageConfig({
 	navigationBarTitleText: "首页",
 	navigationStyle: "custom",
 	enablePullDownRefresh: true,
+	backgroundColor: "#47a5fd",
+	backgroundTextStyle: "light",
 	usingComponents: {},
 });
